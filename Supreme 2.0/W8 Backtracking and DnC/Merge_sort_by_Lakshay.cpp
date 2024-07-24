cc9889a788f3f22f08c61ac540f6b36d02da2c6a
OmkarS25 <collegeomkar0@gmail.com>
2024-07-24 07:37:38 +0530

    Added in-place merge sort

--- Supreme 2.0/W8 Backtracking and DnC/Merge_sort_by_Lakshay.cpp
+++ Supreme 2.0/W8 Backtracking and DnC/Merge_sort_by_Lakshay.cpp
@@ -52,6 +52,38 @@ public:
     }
 };
 
+class Solution1 {
+    // This is the inplace merge sort
+    public:
+    void merge(vector<int> &arr, int l, int mid, int r) {
+        int i = l;
+        int j = mid + 1;
+        while (i <= mid && j <= r) {
+            if (arr[i] <= arr[j]) {
+                i++;
+            } else {
+                int value = arr[j];
+                int index = j;
+
+                while (index != i) {
+                    arr[index] = arr[index - 1];
+                    index--;
+                }
+                arr[i] = value;
+                i++, mid++, j++;
+            }
+        }
+    }
+    void mergeSort(vector<int> &arr, int l, int r) {
+        if(l<r) {
+            int mid = l + (r-l)/2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+            merge(arr, l, mid, r);
+        }
+    }
+};
+
 int main(){
     Solution s;
     vector<int> v = {10, 7, 8, 9, 1, 5};

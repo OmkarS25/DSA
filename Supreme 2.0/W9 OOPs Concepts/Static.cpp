b372c684dcb51add5eb5339a5161770782dc334a
OmkarS25 <collegeomkar0@gmail.com>
2024-08-01 05:55:24 +0530

    Added static member function code

--- Supreme 2.0/W9 OOPs Concepts/Static.cpp
+++ Supreme 2.0/W9 OOPs Concepts/Static.cpp
@@ -4,8 +4,17 @@ using namespace std;
 class abc{
 public:
     static int x,y;
+    int a,b;
     void print() const{
+        // cout<<this->x<<" "<<this->y<<endl; // this keyword will only print the same thing.
         cout<<x<<" "<<y<<endl;
+    }
+    static void print_static(){
+        cout<<x<<" "<<y<<endl;
+        //cout<<a<<" "<<b<<endl; // Static member function can't access non-static members.
+        // No instances can be passed to static member function.
+        // no this pointer is passed to static member function.
+        printf("I am in  static %s\n",/*__func__*/__FUNCTION__);
     }
 };
 
@@ -22,5 +31,10 @@ int main(){
     obj2.y = 20;
     obj1.print();
     obj2.print();
+    // Static members can also be accessed using class name.
+    abc::x = 20;
+    abc::y = 30;
+    // Static member function can also be accessed using class name.
+    abc::print_static();
     return 0;
 }

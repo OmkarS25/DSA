ba9152c720565d6eeff0668bb60d7cb8c7cad552
OmkarS25 <collegeomkar0@gmail.com>
2024-09-03 21:10:29 +0530

    I have made changes to both as the class 1 had class 2 content so I have seperated them.

--- Supreme 2.0/W9 OOPs Concepts/OOPs Class 2/Class_2.cpp
+++ Supreme 2.0/W9 OOPs Concepts/OOPs Class 2/Class_2.cpp
@@ -1,3 +1,10 @@
+/**
+ * @brief In this class we will learn about copy ctor, dtor 
+ * and why copy ctor parameter should be const and address 
+ * of srcobj needs to be passed
+ * @date 2024-09-03
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -47,6 +54,18 @@ public:
         cout << "Student paremeterized ctor w/o gf called" << endl;
     }
 
+    // copy constructor or ctor
+    Student(const Student &srcobj)
+    {
+        cout << "Student copy ctor called" << endl;
+        this->name = srcobj.name;
+        this->age = srcobj.age;
+        this->gf = srcobj.gf;
+        this->nos = srcobj.nos;
+        this->present = srcobj.present;
+        this->id = srcobj.id;
+    }
+
     // getter / setter
     string getGfName()
     {
@@ -72,6 +91,12 @@ public:
         cout << "bunking" << endl;
     }
 
+    ~Student()
+    {
+        cout << "Student dtor called" << endl;
+        delete v;
+    }
+
 private:
     void gfchatting()
     {
@@ -86,35 +111,23 @@ private:
 
 int main()
 {
-    // cout << sizeof(Student) << endl;
-    // Student s1;
+    // copy ctor
+    // Student s1(1, 12, 1, "Chota Bheem", 1, "Chutki");
+    // Student s2 = s1;
+    // Student s2(s1);
 
-    // Student s2(1, 12, 1, "Chota Bheem", 1, "Chutki");
+    // Student s2 = s1;
+
+    // cout << s1.name << endl;
     // cout << s2.name << endl;
-    // cout << s2.id << endl;
 
-    // Student s3(2, 15, 0, "Lokesh", 5);
-    // cout << s3.name << endl;
+    Student s1(1, 12, 1, "Chota Bheem", 1);
+    cout << s1.name << endl;
+    cout << s1.getGfName() << endl;
 
-    // // alocating on heap
-    // Student *s4 = new Student(3, 34, 1, "Chintu", 6);
-    // cout << s4->name << endl;
-    // cout << (*s4).name << endl;
+    s1.setGfName("Indumati");
 
-    // delete s4; // no LEAK
-
-    // s1.name = "Goli";
-    // s1.age = 12;
-    // s1.id = 1;
-    // s1.nos = 5;
-    // s1.present = 1;
-
-    // Student s2;
-    // s2.name = "Chota Bheem";
-    // s2.age = 11;
-    // s2.id = 2;
-    // s2.nos = 1;
-    // s2.present = 0;
+    cout << s1.getGfName() << endl;
 
     return 0;
 }
